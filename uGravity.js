c92c3a70852d98858614dce256da570a1785fb9c
Steven Farthing <sfarthin@gmail.com>
2013-12-02 11:45:10 -0500

    Kepler16 example with center of gravity at the center.

--- uGravity.js
+++ uGravity.js
@@ -11,7 +11,7 @@
 	function uGravity(canvas, opts) {
 	
 		// If canvas is not given lets assume its a data file (jsonp file)
-		if(!opts && typeof canvas == "object") {
+		if(!opts && typeof canvas ==	 "object") {
 			jsonp_opts = canvas;
 			return;
 		}
@@ -25,6 +25,9 @@
 		var self = this;
 		this.canvas = canvas;
 	    this.context2D = canvas.getContext('2d');
+		
+		this.units = opts.units;
+		this.title = opts.title;
 
 		// A reference to the in-memory canvas used as a back buffer 
         this.backBuffer = document.createElement('canvas');
@@ -40,10 +43,28 @@
 		
 		
 		
-		this.offsetX = 0;
-		this.offsetY = 0;
-		this.scale = 80;
+		if(opts.view && opts.view.length == 2) {
+			this.offsetX = opts.view[0];
+			this.offsetY = opts.view[1];
+		}
+
+		this.scale = (opts.scale ? opts.scale : 80);
 		this.mousedown = false;
+		
+
+		// Lets get any graph paper options
+		if(opts.graphpaper) {
+			
+			if(!opts.graphpaper.cellWidth) {
+				this.graphpaper = {};
+				this.graphpaper.segments = (opts.graphpaper.cellsAccross ? opts.graphpaper.cellsAccross : 6);
+				this.graphpaper.cellsAccross = opts.graphpaper.cellsAccross;
+				this.graphpaper.cellWidth = {
+					width: this.canvas.width / opts.graphpaper.cellsAccross,
+					scale: this.scale
+				}
+			}
+		}
 		 		
 		// 	    canvas.addEventListener("dragstart", function( event ) {
 		var last_position_x,
@@ -62,15 +83,20 @@
 		});
 		
 		canvas.addEventListener('mousewheel', function(e) {
-			var wheelData = e.detail ? e.detail * -1 : e.wheelDelta / 10;
+			var wheelData = e.detail ? e.detail * -1 : e.wheelDelta / 1000;
 			
 			// need to scale into the position of the cursor;
-			self.scale += wheelData;
+			self.scale *= 1 + wheelData;
 			if(self.scale < 1)
 				self.scale = 1;
 				
-			// self.offsetX += wheelData;
-			// self.offsetY += wheelData;
+			self.zooming = true;
+			
+			// Lets make new
+			// clearTimeout(this.zoomingTimeout);
+			// this.zoomingTimeout = setTimeout(function() {
+			// 	self.zooming = false;
+			// }, 200);
 				
 
 			e.stopPropagation();
@@ -84,8 +110,8 @@
 	            var y = event.clientY;
 			
 				if(last_position_x && last_position_y) {
-					self.offsetX += (x - last_position_x)/self.scale;
-					self.offsetY += (y - last_position_y)/self.scale;
+					self.offsetX += ((x - last_position_x)/self.scale)*2;
+					self.offsetY += ((y - last_position_y)/self.scale)*2;
 				}
 			
 				last_position_x = x;
@@ -93,12 +119,6 @@
 			}
 			
         });
-	     // }, false);
-			// 		 
-			//  	    canvas.addEventListener("dragend", function( event ) {
-			// canvas.removeEventListener("mousemove");
-			//  	     }, false);
-		
 
 	    /**
 		*
@@ -132,12 +152,6 @@
 				case "render": self.render(data.settings, data.objects); return
 				
 			}
-			//); // Start the worker.
-	
-			// worker.postMessage(this.objects.map(function(g) {
-			// 	g.update = g.update.toString();
-			// 	return g;
-			// }));
 			
 		}
 
@@ -152,14 +166,264 @@
 	        this.backBufferContext2D.clearRect(0, 0, this.backBuffer.width, this.backBuffer.height);
 	        this.context2D.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+			/**
+			*
+			* Application rendering
+			*
+			**/
+			
+			// Lets figure out a good cell size for our graph paper
+			var cellWidth 	= this.graphpaper.cellWidth.width * this.scale / this.graphpaper.cellWidth.scale;
+		
+			// Lets get a scale that about the right amount of cells accross.
+			while(this.canvas.width / cellWidth > this.graphpaper.cellsAccross/2) { cellWidth = cellWidth * 2; }
+			while(this.canvas.width / cellWidth < this.graphpaper.cellsAccross/2) { cellWidth = cellWidth / 2; }
+			this.scaledCellWidth = cellWidth;
+			
+
+			// Draw our graph paper
+			this.drawGraphPaper();
+
 	        // Draw our objects onto the back buffer
 	        for (x in objects) {
 	            this.drawObject(this.backBufferContext2D, objects[x], settings);
 	        }
+			
+			// Draw Scale box in left bottom corner.
+			this.drawScaleBox();
+			
+			// Lets draw labels for objects that are too small
+			this.drawLabels(objects);
+			
+			this.drawTitle(this.title);
+			
+			/**
+			*
+			* Finished rendering our application
+			*
+			**/
     
 	        // copy the back buffer to the displayed canvas
 	        this.context2D.drawImage(this.backBuffer, 0, 0);
 	    };
+		
+		this.drawTitle = function(title) {
+			var ctx 		= this.context2D;			
+
+			ctx.font = '72px Helvetica'; //HelveticaNeue-Light
+			var width 		= ctx.measureText(title).width;
+			
+			ctx.textBaseline = "top"
+			ctx.shadowColor = "#000"
+//			ctx.shadowOffsetX = 4;
+//			ctx.shadowOffsetY = 4;
+			ctx.shadowBlur = 15;
+			ctx.fillStyle = "#FFF";
+			ctx.strokeStyle = "#000";
+			ctx.lineWidth = 7;
+
+			ctx.strokeText(title, this.canvas.width / 2 - width / 2, 20);
+			ctx.fillText(title, this.canvas.width / 2 - width / 2, 20);
+
+			ctx.textBaseline = "bottom";
+			ctx.shadowColor = null;
+			ctx.shadowOffsetX = 0;
+			ctx.shadowOffsetY = 0;
+			ctx.shadowBlur = 0;
+			
+			
+		};
+		
+		this.drawLabels = function(objects) {
+			var ctx = this.backBufferContext2D;
+			
+			var leftEdge 	= (this.scale * this.offsetX) + this.canvas.width/2,
+				rightEdge 	= (this.scale * this.offsetX) - this.canvas.width/2,
+				topEdge 	= (this.scale * this.offsetY) - this.canvas.height/2,
+				bottomEdge  = (this.scale * this.offsetY) + this.canvas.height/2,
+				paintArrow = function(x, y, dir) {
+
+
+					ctx.beginPath();
+					ctx.moveTo(x, y);
+
+					if(dir == "down") {
+						ctx.lineTo(x + 15, y + 30);
+						ctx.lineTo(x, y + 25);
+						ctx.lineTo(x - 15, y + 30);
+						ctx.lineTo(x,y);	
+					} else if(dir == "left") {
+						ctx.lineTo(x + 30, y + 15);
+						ctx.lineTo(x + 25, y);
+						ctx.lineTo(x + 30, y - 15);
+						ctx.lineTo(x,y);
+					} else if(dir == "right") {
+						ctx.lineTo(x - 30, y - 15);
+						ctx.lineTo(x - 25, y);
+						ctx.lineTo(x - 30, y + 15);
+						ctx.lineTo(x,y);
+					} else if(!dir) {
+						ctx.lineTo(x - 15, y - 30);
+						ctx.lineTo(x, y - 25);
+						ctx.lineTo(x + 15, y - 30);
+						ctx.lineTo(x,y);
+					} else {
+						// @todo Use some trig to have arrow wrap around corner.
+						// ------
+					}	// \
+						//	\
+					
+					ctx.fillStyle = object.color;
+					ctx.fill();
+					
+					ctx.lineWidth = 2;
+					ctx.strokeStyle = "#000";
+					ctx.stroke();
+					
+				};
+			
+			for(var i in objects) {
+				var object = objects[i],
+					radius = this.scale * object.radius,
+					x = (this.scale * (object.x + this.offsetX)) + this.canvas.width/2,
+					y = (this.scale * (object.y + this.offsetY)) + this.canvas.height/2,
+					x2 = (this.scale * object.x) * -1,
+					y2 = (this.scale * object.y) * -1;
+				
+					///console.log(topEdge, bottomEdge, y);
+					if(object.name == "Kepler-16A")
+						document.getElementById("debug").innerHTML = [topEdge, bottomEdge, y2].join(", ");
+				
+				if(bottomEdge < y2 - 20) {
+					paintArrow(x,0,"down");
+				} else if(topEdge > y2 + 20) {
+					paintArrow(x,this.canvas.height);
+				} else if(leftEdge < x2) {
+					paintArrow(0,y,"left");
+				} else if(rightEdge > x2) {
+					paintArrow(this.canvas.width,y,"right");
+				} else if(radius < 6) {
+					
+					// upside down arrow ontop of page.
+					// if(topEdge - 150 > y2) {
+					// 	y += radius + 10;
+					// 	paintArrow(x,y,true);
+					// } else {
+						// Lets make sure the arrow is on top of the object.
+						paintArrow(x,y - radius - 10);
+						//}
+
+				}
+				
+				
+				
+			}
+			
+		};
+		
+		this.drawScaleBox = function() {
+			var ctx 		= this.context2D;
+
+			ctx.font = '24px HelveticaNeue-Light';
+			
+			var roundedScale = (Math.round(this.scaledCellWidth / this.scale * 100000) / 100000) + " " + this.units.distance,
+				textWidth 	 = ctx.measureText(roundedScale).width,
+				height 		= 40,
+				padding 	= 20,
+				textPadding	= 8,
+				width = textPadding * 3 + padding + textWidth + this.scaledCellWidth;
+			
+			ctx.fillStyle = "#FFF";
+			ctx.strokeStyle = "black";
+			ctx.shadowBlur = 15;
+			ctx.fillRect(padding,this.canvas.height - height - padding,width,height);
+			ctx.shadowBlur = 0;
+			
+			ctx.lineWidth = 2;
+			ctx.strokeRect(padding, this.canvas.height - height - padding, width, height);
+			
+			ctx.fillStyle = "#000000";
+			ctx.fillText(roundedScale,padding + textPadding + 5,this.canvas.height - padding - textPadding);
+			
+			
+			ctx.lineWidth = 3;
+			ctx.moveTo(padding + 3 * textPadding + textWidth, this.canvas.height - (1.5*padding) - textPadding - 5);
+			ctx.lineTo(padding + 3 * textPadding + textWidth, this.canvas.height - padding - textPadding - 5);
+			ctx.lineTo(padding + 3 * textPadding + textWidth + this.scaledCellWidth, this.canvas.height - padding - textPadding - 5);
+			ctx.lineTo(padding + 3 * textPadding + textWidth + this.scaledCellWidth, this.canvas.height - (1.5*padding) - textPadding - 5);
+			ctx.stroke();
+			
+			
+		};
+		
+		
+		this.drawGraphPaper = function() {
+			var ctx 		= this.context2D,
+				cellWidth 	= this.scaledCellWidth; 
+			
+				// Lets make sure that 0,0 is an intersection of graph lines
+			var yAlignment = (this.canvas.height / 2) % cellWidth,
+				xAlignment = (this.canvas.width  / 2) % cellWidth,
+				
+				// lets determine the first position of the graph lines inside the viewport in respect to the offest from point 0,0
+				startX = (this.offsetX * this.scale) % cellWidth + xAlignment,
+				startY = ((this.offsetY * this.scale) % cellWidth) + yAlignment;
+			
+			/**
+			*
+			* x,y values refer to the x,y cordinates in respect to the canvas
+			*
+			**/
+			
+			for(var x = startX; x <= this.canvas.width; x+= cellWidth) {
+				
+				var point = Math.abs((x - this.canvas.width/2)/this.scale - this.offsetX);
+				
+				if(point < 0.00000000002) {
+					ctx.lineWidth = 0.5;
+				// } else if(Math.floor(point / ((cellWidth + xAlignment)/this.scale)) % 5 < 0.0000002) {
+				// 	ctx.lineWidth = 0.5;
+				} else {
+					ctx.lineWidth = 0.1;
+				}
+			    ctx.beginPath();
+			    ctx.moveTo(x,0);
+			    ctx.lineTo(x,this.canvas.height);
+			    ctx.stroke();
+			}
+			
+			for(var y = startY; y <= this.canvas.height; y+= cellWidth) {
+				
+				var point = Math.abs((y - this.canvas.height/2)/this.scale - this.offsetY);
+				
+				if(point < 0.00000000002) {
+					ctx.lineWidth = 0.5;
+				} else {
+					ctx.lineWidth = 0.1;
+				}
+				
+				ctx.strokeStyle = "black";
+			    ctx.beginPath();
+			    ctx.moveTo(0,y);
+			    ctx.lineTo(this.canvas.width,y);
+			    ctx.stroke();
+			}
+			
+			
+			
+			ctx.beginPath();
+			
+			var centerX = (this.scale * this.offsetX) + this.canvas.width/2,
+				centerY = (this.scale * this.offsetY) + this.canvas.height/2;
+			
+			ctx.arc(centerX,centerY,3,0,Math.PI*2,true); // Outer circle
+			
+			//context.fillStyle = '#8ED6FF';
+			ctx.fill();
+			
+			
+			
+		}
 
 		/**
 		*
@@ -170,7 +434,7 @@
 
 			object.scaledX 		= (this.scale * (object.x + this.offsetX)) + this.canvas.width/2;
 			object.scaledY 		= (this.scale * (object.y + this.offsetY)) + this.canvas.height/2;
-			object.scaledRadius = this.scale * object.radius * 20;
+			object.scaledRadius = this.scale * object.radius;
 	
 			ctx.save();
 
@@ -200,7 +464,7 @@
 		**/		
 		var SETTINGS = {
 				FPS: 30,
-				TIME_SCALE: 1
+				TIME_SCALE: 0.5
 				// SECONDS_BETWEEN_FRAMES is computed
 			},
 			OBJECTS = [],
